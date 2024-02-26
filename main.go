cb6fc4389860cb54c98f2dcd3f2d94cd99e57082
irumaru <jibakomaorochi13@outlook.jp>
2024-02-26 07:36:35 +0000

    Add schema

---
+++ main.go
@@ -0,0 +1,55 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"log"
+
+	"github.com/go-sql-driver/mysql"
+	"github.com/irumaru/go-ent-intro/ent"
+)
+
+func main() {
+	//jst, _ := time.LoadLocation("Asia/Tokyo")
+
+	dcs := mysql.Config{
+		Addr:                 "db:3306",
+		User:                 "dev",
+		DBName:               "dev",
+		Passwd:               "dev",
+		Net:                  "tcp",
+		AllowNativePasswords: true,
+		Collation:            "utf8mb4_general_ci",
+	}
+
+	//fmt.Println(dcs.FormatDSN())
+
+	// Open the connection to the database (MySQL).
+	client, err := ent.Open("mysql", dcs.FormatDSN())
+	if err != nil {
+		log.Fatalf("failed opening connection to mysql: %v", err)
+	}
+	defer client.Close()
+
+	// Run the auto migration tool.
+	if err := client.Schema.Create(context.Background()); err != nil {
+		log.Fatalf("failed creating schema resources: %v", err)
+	}
+
+	// Create a new user.
+	u, _ := CreateUser(context.Background(), client)
+	log.Println(u)
+}
+
+func CreateUser(ctx context.Context, client *ent.Client) (*ent.User, error) {
+	u, err := client.User.
+		Create().
+		SetAge(16).
+		SetName("a8m").
+		Save(ctx)
+	if err != nil {
+		return nil, fmt.Errorf("failed creating user: %v", err)
+	}
+	log.Println("user was created: ", u)
+	return u, nil
+}
